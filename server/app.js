deef3d32294c9fd12ad75fa83eb16468868f2100
Dienul <haqdienul@gmail.com>
2019-05-22 18:12:13 +0700

    push kedua

--- server/app.js
+++ server/app.js
@@ -1,16 +1,23 @@
+require('dotenv').config()
 var cors = require('cors')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const routes = require('./routes')
 const mongoose = require('mongoose')
+const connectingMonggoDb = process.env.connectingMonggoDb || 'mongodb://localhost/hacktivoverflow'
 
 
 mongoose.set('useNewUrlParser', true)
-
-mongoose.connect(process.env.MONGOLINK,{useNewUrlParser: true})
-// mongodb+srv://<username>:<password>@cluster0-jpvto.gcp.mongodb.net/test?retryWrites=true
-require('dotenv').config()
+mongoose.connect(connectingMonggoDb,{useNewUrlParser: true})
+.then(() => {
+  console.log("Connected to database!");
+  })
+  .catch((error) => {
+  console.log(`bermasalah`);
+  console.log("Connection failed!");
+  console.log(error);
+  })
 
 app.use(cors())
 
