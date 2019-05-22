95749ebc8ae0034223d07d3403f01806039edca8
Dienul <haqdienul@gmail.com>
2019-05-22 18:22:49 +0700

    push ketiga

--- client/src/api/backend.js
+++ client/src/api/backend.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'http://35.187.238.131/',
   timeout: 10000,
   withCredentials: false,
 })
