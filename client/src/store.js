9f414299f6da56633269d0e260a695f713cdc7db
m43max <maxpt750@gmail.com>
2019-05-24 15:27:57 +0700

    updated server url in client

--- client/src/store.js
+++ client/src/store.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    serverAddress: "http://localhost:3000/",
+    serverAddress: "http://104.155.210.171:3000/",
     userEmail: "",
     userId: "",
     loggedIn: false,
